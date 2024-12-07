bcce6b8f5c9662697c7fe2aee077d59de4c93d0b
forkhelloworld <serhiisliusarskyi@gmail.com>
2024-12-07 18:55:40 +0200

    add: models

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  birthday: Date,
+  imagePath: String
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
