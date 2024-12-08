e3fa518868d62291af466c77285b0597172f1e52
forkhelloworld <serhiisliusarskyi@gmail.com>
2024-12-08 16:57:46 +0200

    add: socket.io

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
-const http = require('http');
-const app = require('./app.js');
+const http = require("http");
+const { Server } = require("socket.io");
+const app = require("./app.js");
+const { createWebsocketConnect } = require("./webSocket");
 
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", createWebsocketConnect);
 
 const PORT = 5000;
 
 server.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
-})
+});
